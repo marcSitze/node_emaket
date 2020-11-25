98cff69027a92840be776a462c26eff2916eaebe
theblackdev <61018624+marcSitze@users.noreply.github.com>
2020-11-25 11:25:27 +0000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/node_emarket, {useNewUrlParser: true, useUnifiedTopology: true}'
+    database: 'mongodb://localhost:27017/node_emarket'
 
 }
